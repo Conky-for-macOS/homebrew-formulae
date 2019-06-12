2316780d357024d662a9d8f02e028984a6e974a1
npyl <n.pylarinos@hotmail.com>
2019-06-12 20:57:45 +0300

    Update libircclient.rb
    Ok, this was needed.

--- Formula/libircclient.rb
+++ Formula/libircclient.rb
@@ -5,10 +5,10 @@ class Libircclient < Formula
   url 'https://github.com/Conky-for-macOS/libircclient/archive/v1.10.1.tar.gz'
   sha256 'b4b7eac7b40f8e9aede3af8b035730b3436c0b8536b7613a72e57de0bb8d653e'
 
-  depends_on 'pkg-config'
-  depends_on 'autoconf'
-  depends_on 'automake'
-  depends_on 'libtool'
+  depends_on 'pkg-config' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
 
   def install
     system "./configure --prefix=#{prefix} --enable-shared"
