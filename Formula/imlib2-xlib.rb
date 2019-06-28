99cc87e364cbebf0adeb0d39d038d5cb523b7418
npyl <n.pylarinos@hotmail.com>
2019-06-29 00:02:27 +0300

    Update imlib2-xlib.rb
    Disable bottles.
    There is no imlib2 bottle with xlib-support.

--- Formula/imlib2-xlib.rb
+++ Formula/imlib2-xlib.rb
@@ -4,13 +4,6 @@ class Imlib2Xlib < Formula
   url "https://downloads.sourceforge.net/project/enlightenment/imlib2-src/1.5.1/imlib2-1.5.1.tar.bz2"
   sha256 "fa4e57452b8843f4a70f70fd435c746ae2ace813250f8c65f977db5d7914baae"
 
-  bottle do
-    rebuild 1
-    sha256 "f2bb2e5ca54865edf5808245b54b0b636669e496757989bd4d6a19dc098b152b" => :mojave
-    sha256 "0349423dda370df2ec72d02ff5d818f0f7c34197b9d6661212ae03c14b6dfbb0" => :high_sierra
-    sha256 "1bebf0a2a3e58f6766e32fe6cbafa8f478788eb9aaed64204fa113204981a3f5" => :sierra
-  end
-
   depends_on "pkg-config" => :build
   depends_on "freetype"
   depends_on "giflib"
