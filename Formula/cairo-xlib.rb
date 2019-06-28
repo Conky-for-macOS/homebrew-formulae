8b38815be9883003b0967bb6049c948b0aefd136
npyl <n.pylarinos@hotmail.com>
2019-06-28 21:54:26 +0300

    Update cairo-xlib.rb
    Enable gobject.
    This is done to ensure we match the default cairo behaviour.

--- Formula/cairo-xlib.rb
+++ Formula/cairo-xlib.rb
@@ -29,7 +29,7 @@ class CairoXlib < Formula
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--enable-gobject=no",
+                          "--enable-gobject=yes",
                           "--enable-svg=yes",
                           "--enable-tee=yes",
                           "--enable-quartz-image",
