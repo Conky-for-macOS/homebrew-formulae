0fad64a1fa8e15c0bdfe2054d20e661f6bc6f3d0
TheGamer1002 <48131223+TheGamer1002@users.noreply.github.com>
2021-02-03 11:32:40 -0500

    Update imlib2-xlib.rb

--- Formula/imlib2-xlib.rb
+++ Formula/imlib2-xlib.rb
@@ -10,7 +10,7 @@ class Imlib2Xlib < Formula
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "libtiff"
-  depends_on :x11
+  depends_on "xquartz"
 
   keg_only "An imlib2 installation may already be present."
 
